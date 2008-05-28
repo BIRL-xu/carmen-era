a695e3b556a0837de98ca9267d981f8cf3dae570
Dizan Vasquez <dichodaemon@gmail.com>
2008-05-28 21:27:19 +0000

    * Added complete tests test scripts for the robot arms.

--- trunk/carmen-era-python/sandbox/arm_config_pub.py
+++ trunk/carmen-era-python/sandbox/arm_config_pub.py
@@ -1,11 +1,33 @@
 from era_comm import *
+from time import sleep
 
 p = ArmConfigPublisher()
 
-i = 0
+def read_data( filename ):
+  f = open( filename, 'r' )
+  result = []
+  for line in f:
+    values = line.rsplit()
+    conf = []
+    for v in values:
+      conf.append( float( v ) )
+    result.append( conf )
+  f.close()
+  return result
+
+configs   = read_data( "configs.dat"  )
+
 
 while True:
-  p.shoulder_yaw = i
+  c = configs.pop( 0 )
+  time = configs[0][5]
+  p.shoulder_yaw   = c[0]
+  p.shoulder_roll  = c[1]
+  p.shoulder_pitch = c[2]
+  p.ellbow_pitch   = c[3]
+  p.tool_roll      = c[4]
+  configs.append( c )
   print p.shoulder_yaw
   p.send()
-  i += 1
+  sleep( time )
+
