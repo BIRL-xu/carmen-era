6c2490c7cbd1dee237efd4130b5eac79c0e34a52
Ralf Kaestner <ralf.kaestner@gmail.com>
2009-06-15 10:40:04 +0000

    CARMEN interface for the arm accepts position commands

--- trunk/carmen/era/era.c
+++ trunk/carmen/era/era.c
@@ -25,6 +25,8 @@
 
 #include <base/era.h>
 #include <motors/home.h>
+#include <dynamics/dynamics.h>
+#include <motors/position_profile.h>
 #include <control/sensors.h>
 
 #include "era_params.h"
@@ -34,6 +36,9 @@
 carmen_era_joint_cmd_message carmen_era_joint_cmd;
 
 int carmen_era_quit = 0;
+
+era_arm_t carmen_era_arm;
+thread_mutex_t carmen_era_mutex;
 
 void carmen_era_signaled(int signal) {
   if (signal == SIGINT)
@@ -95,29 +100,71 @@ void carmen_era_sensors_handle(era_joint_state_p joint_state,
   era_velocity_state_p vel_state, double freq __attribute__ ((unused))) {
   double timestamp = carmen_get_time();
 
-  carmen_era_publish_joint_state(joint_state, timestamp);
-  carmen_era_publish_velocity_state(vel_state, timestamp);
+  carmen_era_publish_joint_state(joint_state->shoulder_yaw, 
+    joint_state->shoulder_roll, joint_state->shoulder_pitch, 
+    joint_state->elbow_pitch, joint_state->tool_roll, 
+    joint_state->tool_opening, timestamp);
+  carmen_era_publish_velocity_state(vel_state->shoulder_yaw, 
+    vel_state->shoulder_roll, vel_state->shoulder_pitch, 
+    vel_state->elbow_pitch, vel_state->tool_roll, vel_state->tool_opening, 
+    timestamp);
 
   fprintf(stderr, "S");
   fflush(stderr);
 }
 
 void carmen_era_joint_cmd_handle(void* joint_cmd) {
+  era_joint_state_t start_state;
+  era_joint_state_t target_state;
+  era_velocity_state_t vel_state;
+  era_motors_position_profile_t profile;
+  carmen_era_joint_cmd_message* message = joint_cmd;
+  int result;
+
   fprintf(stderr, "C");
   fflush(stderr);
+
+  thread_mutex_lock(&carmen_era_mutex);
+  era_motors_position_profile_stop(&carmen_era_arm.motors);
+  thread_mutex_unlock(&carmen_era_mutex);
+
+  target_state.shoulder_yaw = message->shoulder_yaw;
+  target_state.shoulder_roll = message->shoulder_roll;
+  target_state.shoulder_pitch = message->shoulder_pitch;
+  target_state.elbow_pitch = message->elbow_pitch;
+  target_state.tool_roll = message->tool_roll;
+  target_state.tool_opening = message->tool_opening;
+  
+  if ((result = era_kinematics_limits_test_state(&carmen_era_arm.kin_limits, 
+    &target_state))) {
+    fprintf(stderr, "\n%s\n", era_kinematics_limits_errors[result]);
+    return;
+  }
+    
+  era_get_joint_state(&carmen_era_arm, &start_state);
+  era_dynamics_limit_state(&start_state, &target_state, 
+    &carmen_era_arm.dyn_limits, message->vel_factor, &vel_state);
+
+  era_motors_position_profile_init(&profile, &target_state, &vel_state,
+    &carmen_era_arm.dyn_limits.max_accel, &carmen_era_arm.dyn_limits.min_accel, 
+    epos_profile_sinusoidal);
+
+  thread_mutex_lock(&carmen_era_mutex);
+  era_motors_position_profile_start(&carmen_era_arm.motors, &profile);
+  thread_mutex_unlock(&carmen_era_mutex);
 }
 
 void carmen_era_wait_commands(era_arm_p arm, double freq) {
   thread_t sensor_thread;
-  thread_mutex_t mutex;
 
   fprintf(stderr, "Now accepting motion commands.\n\n");
-  era_control_sensors_start(&sensor_thread, arm, &mutex, 
+  era_control_sensors_start(&sensor_thread, arm, &carmen_era_mutex, 
     carmen_era_sensors_handle, freq);
+  timer_sleep(1.0);
   carmen_era_subscribe_joint_cmd_message(&carmen_era_joint_cmd,
     carmen_era_joint_cmd_handle, CARMEN_SUBSCRIBE_LATEST);
 
-  while (!carmen_era_quit) timer_sleep(0.1);
+  while (!carmen_era_quit) carmen_ipc_sleep(0.1);
   fprintf(stderr, "\n");
 
   carmen_era_unsubscribe_joint_cmd_message(carmen_era_joint_cmd_handle);
@@ -127,7 +174,6 @@ void carmen_era_wait_commands(era_arm_p arm, double freq) {
 int main(int argc, char **argv) {
   config_t can_config;
   era_config_t era_config;
-  era_arm_t arm;
   double freq;
 
   config_init(&can_config);
@@ -138,17 +184,20 @@ int main(int argc, char **argv) {
 
   can_device_p can_dev = malloc(sizeof(can_device_t));
   can_init(can_dev, &can_config);
-  era_init(&arm, can_dev, &era_config);
+  era_init(&carmen_era_arm, can_dev, &era_config);
 
   config_destroy(&can_config);
   era_config_destroy(&era_config);
 
   signal(SIGINT, carmen_era_signaled);
 
-  if (!carmen_era_connect(&arm) /*&& !carmen_era_home(&arm)*/)
-    carmen_era_wait_commands(&arm, freq);
+  thread_mutex_init(&carmen_era_mutex);
+  if (!carmen_era_connect(&carmen_era_arm) && 
+    !carmen_era_home(&carmen_era_arm))
+    carmen_era_wait_commands(&carmen_era_arm, freq);
+  thread_mutex_destroy(&carmen_era_mutex);
 
-  era_close(&arm);
-  era_destroy(&arm);
+  era_close(&carmen_era_arm);
+  era_destroy(&carmen_era_arm);
   return 0;
 }
